80adc0eabdeca74d1608077416fe199c714f3dd2
zach.lewis_clarity <zlewis@clarityinnovates.com>
2025-03-18 10:52:49 -0500

    add updates

--- setup/init.sh
+++ setup/init.sh
@@ -11,6 +11,7 @@ git checkout develop
 echo "# Development Branch" >> README.md
 git add .
 git commit -m "Setup develop branch"
+git push --set-upstream origin develop
 
 # Create and setup staging branch
 git branch staging
@@ -18,6 +19,7 @@ git checkout staging
 echo "# Staging Branch" >> README.md
 git add .
 git commit -m "Setup staging branch"
+git push --set-upstream origin staging
 
 # Create and setup integration branch
 git branch integration
@@ -25,6 +27,7 @@ git checkout integration
 echo "# Integration Branch" >> README.md
 git add .
 git commit -m "Setup integration branch"
+git push --set-upstream origin integration
 
 # Create and setup prod branch
 git branch prod
@@ -32,4 +35,5 @@ git checkout prod
 echo "# Production Branch" >> README.md
 git add .
 git commit -m "Setup prod branch"
+git push --set-upstream origin prod
 
